2d8d7afdff078e7c2943ae8769aa09a4550f17b7
Anonymous549 <animeamvtv531@gmail.com>
2024-05-17 23:56:11 +0530

    Update AwtGui.java

--- AwtGui.java
+++ AwtGui.java
@@ -5,14 +5,16 @@ public class AwtGui {
         frame.setSize(420,420);
         frame.setTitle("HELLO WORLD!");
         // Setting the exit behavior
-                frame.addWindowListener(new WindowAdapter() {
-                            public void windowClosing(WindowEvent e) {
-                                            // Define actions to perform when the window is closing
-                                                            System.exit(0); // Exiting the application
-                                                                        }
-                                                                                });
+        frame.addWindowListener(new WindowAdapter() {
+        public void windowClosing(WindowEvent e) {
+                                            
+        // Define actions to perform when the window is closing
+                                                            
+        System.exit(0); // Exiting the application
+        }
+        });
                                                                                         
-                                                                                                // Other frame setup code to
+        // Other frame setup code to
                                                             
         frame.setLayout(null);
         frame.setVisiable(true);
