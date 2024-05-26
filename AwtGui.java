997de74493451b32d243123faba81b06a5d10ba2
Anonymous549 <animeamvtv531@gmail.com>
2024-05-26 23:46:21 +0530

    Update AwtGui.java

--- AwtGui.java
+++ AwtGui.java
@@ -59,7 +59,7 @@ public class AwtGui {
         @Override
         public void actionPerformed(ActionEvent e) {
             label.setText("Button Clicked Successfully");
-            label.setBounds(200,240,200,50);
+            label.setBounds(200,240,150,50);
         }
         });
 
